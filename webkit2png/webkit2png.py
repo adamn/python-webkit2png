9f75fc2e9a8da01d4367d0ce81a6de798739ba10
Frédéric BISSON <zigazou@free.fr>
2015-02-09 11:57:18 +0100

    import os forgotten

--- webkit2png/webkit2png.py
+++ webkit2png/webkit2png.py
@@ -29,6 +29,7 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
 from PyQt4.QtNetwork import *
+import os
 
 # Class for Website-Rendering. Uses QWebPage, which
 # requires a running QtGui to work.
